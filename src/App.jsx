246232761ff99f06f7dc1020d7ac59333064cfc6
Rahul Singh <rahul124.singhmy@gmail.com>
2024-10-10 14:30:42 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import TaskInput from "./components/TaskInput";
+import TaskItem from "./components/taskItem";
+
+
+function App() {
+  const [toDoList, setToDoList] =useState([]);
+
+  const addTask = (taskName) =>{
+    const newTask = { taskName, checked: false };
+    setToDoList([...toDoList, newTask]);
+  
+  
+  };
+
+  function deleteTask(deleteTaskName) {
+    setToDoList(toDoList.filter((task) => task.
+    taskName !== deleteTaskName));
+  }
+  
+  function toggleCheck(taskName)  {
+    setToDoList((prevToDoList) =>
+      prevToDoList.map((task)  => 
+        task.taskName === taskName ? { ...task,
+          checked: !task.checked } : task,
+      ),
+    );
+  }    
+
+  console.log(toDoList);
+
+  return ( 
+  <>
+    <div className="container">
+    <h1>2DO:Make🗓️ Today Epic🌟 </h1> 
+    
+    <TaskInput addTask={addTask} />
+
+    <div className="toDoList">
+      <span>To Do</span>
+      <ul className="list-items">
+        {toDoList.map((task, key) => ( 
+          <TaskItem 
+          task={task} 
+          key={key}
+          deleteTask={deleteTask} 
+          toggleCheck={toggleCheck}
+          />
+        ))}
+
+      </ul>
+      
+      {toDoList.length === 0 ? (
+        <p className="notify">You are done!</p>
+
+      ) : null}
+
+    </div>
+
+  </div> 
+ 
+  
+    </>
+  );
+  
+} 
+
+export default App;
---
+++ src/components/TaskInput.jsx
@@ -0,0 +1,28 @@
+import React, {useState} from 'react';
+
+const TaskInput = ({ addTask }) => {
+    const [task, setTask] = useState('');
+
+  //  console.log(task);
+    function handleInputValue(event){
+        setTask(event.target.value);
+
+    }
+
+    function handleAddTask(event){
+      
+       event.preventDefault(); 
+       if (task.trim() === '') return;
+        addTask(task);
+        setTask('');
+    }
+  return <form className="inputField" onSubmit=
+  {handleAddTask}>
+        <input type="text" 
+        id="new-task" placeholder="Enter your new task" 
+        onChange={handleInputValue} />
+        <button>+</button>
+    </form>
+};
+
+export default TaskInput;
